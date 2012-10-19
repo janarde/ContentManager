a29f85efa0d9c4452e16c7fe02fc843ed7583427
Jesse_Anarde <Jesse_Anarde@SE-JAnarde-DT.nuance.com>
2012-10-19 14:09:53 -0700

    refactoring to convert venue object to xml to then marshal into JSON

--- GzCSVToJSON/src/main/java/com/mobile/grazie/csv/Converter.java
+++ GzCSVToJSON/src/main/java/com/mobile/grazie/csv/Converter.java
@@ -6,6 +6,8 @@ import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.mobile.grazie.csv.Venue;
+
 import au.com.bytecode.opencsv.CSVReader;
 
 import com.thoughtworks.xstream.XStream;
@@ -16,8 +18,8 @@ public class Converter {
 	
 	
 	public static void main(String[] args) {
-		String startFile = "C:\\workspace\\GzCSVToJSON\\resources\\bl-locations.csv";
-		String outFile = "C:\\workspace\\GzCSVToJSON\\resources\\bl-locations.xml";
+		String startFile = "C:\\Users\\Jesse_Anarde\\git\\ContentManager\\GzCSVToJSON\\resources\\bl-locations.csv";
+		String outFile = "C:\\Users\\Jesse_Anarde\\git\\ContentManager\\GzCSVToJSON\\resources\\bl-locations.xml";
 		
 		try {
 			CSVReader reader = new CSVReader(new FileReader(startFile));
@@ -25,23 +27,53 @@ public class Converter {
 			
 			String[] header = reader.readNext();
 			
+			for (String h: header) {
+				System.out.println(h);
+			}
 			// make it a list of venues
-			List<Object> out = new ArrayList<Object>();
+			List<Venue> out = new ArrayList<Venue>();
 			
 			while ((line = reader.readNext()) != null) {
-				List<String[]> item = new ArrayList<String[]>();
+				
 				for (int i = 0; i < header.length; i++ ) {
-					String[] keyVal = new String[2];
-					String string = header[i];
-					String val = line[i];
-					keyVal[0] = string;
-					keyVal[1] = val;
-					item.add(keyVal);
+					System.out.println("populating " + header[i] + " with " + line[i]);
+					Venue v = new Venue();
+					if (header[i] == "StoreID") {
+						System.out.println("got here setting storeId = " + line[i]);
+						v.setStoreID(line[i]);
+					} else if (header[i] == "StoreName") {
+						v.setStoreName(line[i]);
+					} else if (header[i] == "Address") {
+						v.setAddress(line[i]);
+					} else if (header[i] == "City") {
+						v.setCity(line[i]);
+					} else if (header[i] == "State") {
+						v.setState(line[i]);
+					} else if (header[i] == "Zip") {
+						v.setZip(line[i]);
+					} else if (header[i] == "PhoneNumber") {
+						v.setPhoneNumber(line[i]);
+					} else if (header[i] == "Latitude") {
+						v.setLatitude(line[i]);
+					} else if (header[i] == "Longitude") {
+						v.setLongitude(line[i]);
+					} else if (header[i] == "Type") {
+						v.setType(line[i]);
+					} else if (header[i] == "FacebookPlaceID") {
+						v.setFbLocId(line[i]);
+					} else if (header[i] == "Logo") {
+						v.setLogo(line[i]);
+					} else {
+						System.out.println("no match");
+					}
+					out.add(v);
 				}
-				out.add(item);
+				
 			}
 			
 			XStream xstream = new XStream();
+			xstream.alias("Venues", List.class);
+			xstream.alias("venue", Venue.class);
 			xstream.toXML(out, new FileWriter(outFile, false));
 			
 		} catch (Exception e) {
